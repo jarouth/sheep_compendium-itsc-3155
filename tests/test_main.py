db3a3e9c2168b097bbb8765e6f34ca6990c5bd00
jason routh <jasonrouth2005@gmail.com>
2024-10-28 16:10:11 -0400

    test case for update sheep

--- tests/test_main.py
+++ tests/test_main.py
@@ -28,10 +28,11 @@ def test_add_sheep():
     }
     response = client.post("/sheep/", json=new_sheep_data)
     assert response.status_code == 201
-    
+
     assert response.json() == new_sheep_data
     new_sheep = client.get(f"/sheep/{new_sheep_data['id']}")
     assert new_sheep.status_code == 200
+
 
 def test_delete_sheep():
     sheep_to_delete = {
@@ -42,10 +43,34 @@ def test_delete_sheep():
     }
     response = client.post("/sheep/", json=sheep_to_delete)
     assert response.status_code == 201
-    
+
     assert response.json() == sheep_to_delete
     new_sheep = client.delete(f"/sheep/{sheep_to_delete['id']}")
     assert new_sheep.status_code == 204
-    
+
     get_response = client.delete(f"/sheep/{sheep_to_delete['id']}")
     assert get_response.status_code == 404
+
+
+def test_update_sheep():
+    sheep_to_update = {
+        "id": 12,
+        "name": "F2",
+        "breed": "Merino",
+        "sex": "ewe"
+    }
+    response = client.post("/sheep/", json=sheep_to_update)
+    assert response.status_code == 201
+    updated_sheep_data = {
+        "id": 12,
+        "name": "Snowball",
+        "breed": "Dorset",
+        "sex": "ram"
+    }
+    response = client.put(
+        f"/sheep/{updated_sheep_data['id']}", json=updated_sheep_data)
+    assert response.status_code == 200
+    
+    get_response = client.get(f"/sheep/{updated_sheep_data['id']}")
+    assert get_response.status_code == 200
+    assert get_response.json() == updated_sheep_data
